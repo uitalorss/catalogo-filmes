12c6ad4f97d6c64f9fca4914627e440864e20013
Ine Ballardin <ineballardin@gmail.com>
2024-01-04 14:14:39 -0300

    test: :white_check_mark: Refatoração de Imports e Correção de Erros de Injeção de Dependências
    Ao invés de usar caminhos absolutos eu importei seguindo caminhos relativos porque eles são mais flexíveis se você decidir reorganizar seus arquivos no futuro. Resolvi erros de injeção de dependencias de serviços que precisavam ser fornecidos e estavam faltando.

--- src/films/films.controller.spec.ts
+++ src/films/films.controller.spec.ts
@@ -1,20 +1,54 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { FilmsController } from './films.controller';
 import { FilmsService } from './films.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Film } from './entities/film.entity';
+import { Genre } from './entities/genre.entity';
+import { Artist } from './entities/artist.entity';
+import { ContentRating } from './entities/contentRating.entity';
+import { Repository } from 'typeorm';
+import { JwtService } from '@nestjs/jwt';
 
 describe('FilmsController', () => {
   let controller: FilmsController;
+  let service: FilmsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [FilmsController],
-      providers: [FilmsService],
+      providers: [
+        FilmsService,
+        {
+          provide: getRepositoryToken(Film),
+          useClass: Repository,
+        },
+        {
+          provide: getRepositoryToken(Genre),
+          useClass: Repository,
+        },
+        {
+          provide: getRepositoryToken(Artist),
+          useClass: Repository,
+        },
+        {
+          provide: getRepositoryToken(ContentRating),
+          useClass: Repository,
+        },
+        {
+          provide: JwtService,
+          useValue: {
+            sign: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<FilmsController>(FilmsController);
+    service = module.get<FilmsService>(FilmsService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+    expect(service).toBeDefined();
   });
 });

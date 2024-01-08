fb137175e7c42a1b85d62faf6b3130858b4be9eb
Uítalo <uitalo.urss@gmail.com>
2024-01-08 16:47:04 -0300

    Ajuste para não permitir uso das rotas para filmes sem autenticação

--- src/films/films.controller.ts
+++ src/films/films.controller.ts
@@ -9,6 +9,7 @@ import {
   HttpCode,
   Put,
   Query,
+  UseGuards,
 } from '@nestjs/common';
 import { FilmsService } from './films.service';
 import { CreateFilmDto, createFilmSchema } from './dto/create-film.dto';
@@ -16,11 +17,13 @@ import { instanceToInstance } from 'class-transformer';
 import { ZodValidationPipe } from '../users/helpers/ZodValidationPipe';
 import { UpdateFilmDTO, partialFilmSchema } from './dto/update-film.dto';
 import { searchQueryDto, searchQuerySchema } from './dto/search-query.dto';
+import { authGuard } from '../auth/auth.guard';
 
 @Controller('films')
 export class FilmsController {
   constructor(private readonly filmsService: FilmsService) {}
 
+  @UseGuards(authGuard)
   @Post()
   @HttpCode(201)
   public async create(
@@ -31,6 +34,7 @@ export class FilmsController {
     return res.json(film);
   }
 
+  @UseGuards(authGuard)
   @Get()
   public async findAll(
     @Query(new ZodValidationPipe(searchQuerySchema)) query: searchQueryDto,
@@ -41,12 +45,14 @@ export class FilmsController {
     return res.json(instanceToInstance(films));
   }
 
+  @UseGuards(authGuard)
   @Get(':id')
   public async findOne(@Res() res, @Param('id') id: string) {
     const film = await this.filmsService.findOne(id);
     return res.json(instanceToInstance(film));
   }
 
+  @UseGuards(authGuard)
   @HttpCode(204)
   @Put(':id')
   update(
@@ -59,6 +65,7 @@ export class FilmsController {
     return res.send();
   }
 
+  @UseGuards(authGuard)
   @HttpCode(204)
   @Delete(':id')
   public async remove(@Res() res, @Param('id') id: string) {
